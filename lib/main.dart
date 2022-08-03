4296ec4e1268b950bdb46894a5951b02a4042efb
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-03 17:23:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,44 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Column(children: [ElevatedButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder:(context)=>GreyPage()));
-          },
-          style: ElevatedButton.styleFrom(primary: Colors.grey), 
-          child: Text("Enter the grey page"),),
-ElevatedButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder:(context)=>PurplePage()));
-          },
-          style: ElevatedButton.styleFrom(primary: Colors.purple), 
-          child: Text("Enter the Purple page"),)
-          ],
-          )
+    return MaterialApp(title: 'Material App', home: MainMenu());
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              if (Navigator.canPop(context)) {
+                print("maybe");
+              } else
+                print("no");
+              // Navigator.of(context).maybePop();
+            },
+            style: ElevatedButton.styleFrom(primary: Colors.grey),
+            child: Text("Enter the grey page"),
           ),
-        ),
-      );
-    
+          ElevatedButton(
+            onPressed: () async {
+              int? _gelen = await Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => PurplePage()));
+              print("main sayı= $_gelen");
+            },
+            style: ElevatedButton.styleFrom(primary: Colors.purple),
+            child: Text("Enter the Purple page"),
+          )
+        ],
+      )),
+    );
   }
 }
