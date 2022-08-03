dc827593aeed7bfcfca9e27db7558e229c48ed05
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-03 17:23:35 +0300

    Update purple_page.dart

--- lib/purple_page.dart
+++ lib/purple_page.dart
@@ -1,20 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class PurplePage extends StatelessWidget {
-  const PurplePage({Key? key}) : super(key: key);
-
+  PurplePage({Key? key}) : super(key: key);
+  int _randomSayi = 0;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text("PruplePage"),
-        backgroundColor: Colors.grey,
+    return WillPopScope(
+      onWillPop: () {
+        print("No");
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("PruplePage"),
+          backgroundColor: Colors.grey,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Pruple Page",
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _randomSayi = Random().nextInt(99);
+                  print("sayı = $_randomSayi");
+                  Navigator.of(context).pop(_randomSayi);
+                },
+                child: Text("Back"))
+          ],
+        )),
       ),
-      body: Center(
-          child: Text(
-        "Pruple Page",
-        style: TextStyle(fontSize: 24),
-      )),
     );
   }
 }
